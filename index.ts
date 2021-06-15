8a97a0f01985d8921980b1a9ab15f213a366919e
Manuel Duarte <carlosd99m@gmail.com>
2021-06-14 22:00:43 -0600

    add plugins oak :sparkles:

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World")
+import { Application, Router } from "https://deno.land/x/oak@v7.5.0/mod.ts";
+
+const router = new Router();
+
+const app = new Application();
+
+router.get("/", ({ response }) => {
+  response.body = "Helo world";
+});
+
+app.use(router.routes());
+
+console.log("server running on port", 300);
+await app.listen({ port: 3000 });
